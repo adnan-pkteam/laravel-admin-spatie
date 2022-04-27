554582efbaa0ecc4c616b7bdb29be73035ea216a
adnan-pkteam <m.adnan@knowpakistan.org>
2022-04-27 07:28:13 +0500

    role and permission

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,51 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+        <div class="w-full m-2">
+            @can('write post')
+            <a href="#" class="m-2 p-2 bg-green-400 rounded">
+                Create Post
+            </a>
+            @endcan
+        </div>
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div class="overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                  <thead class="bg-gray-50 dark:bg-gray-600 dark:text-gray-200">
+                    <tr>
+                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Id</th>
+                      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Title</th>
+                    </tr>
+                  </thead>
+                  <tbody class="bg-white divide-y divide-gray-200">
+                    <tr></tr>
+                    @foreach (App\Models\Post::all() as $post)
+                        
+                    <tr>
+                      <td class="px-6 py-4 whitespace-nowrap">{{ $post->id}}</td>
+                      <td class="px-6 py-4 whitespace-nowrap">{{ $post->title}}</td>
+                      <td class="px-6 py-4 text-right text-sm">
+                        @can('edit post')
+                        <a href="#" class="m-2 p-2 bg-green-400 rounded">
+                            Edit
+                        </a>
+                        @endcan
+                        @can('publish post')
+                            <a href="#" class="m-2 p-2 bg-green-400 rounded">
+                                Publish
+                            </a>
+                        @endcan
+                        
+                    </td>
+                    </tr>
+                    @endforeach
+
+                    <!-- More items... -->
+                  </tbody>
+                </table>
+                <div class="m-2 p-2">Pagination</div>
+              </div>
             </div>
         </div>
     </div>
